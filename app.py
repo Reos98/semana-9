6a17f4949d0d5d072c8ef9d43228974be4654e6d
Reos98 <bl.montoyab@uea.edu.ec>
2026-02-22 20:10:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# RUTA PRINCIPAL
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# RUTA ACERCA DE
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+# RUTA DINÁMICA DE TURNOS
 @app.route('/cita/<paciente>')
 def ver_cita(paciente):
     return render_template('turno.html', paciente=paciente)
